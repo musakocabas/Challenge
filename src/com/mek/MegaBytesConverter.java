83190768bb5ebf51994a901b6bc0874d7e8dd61d
Musa Kocabas <mek26mek@gmail.com>
2021-03-14 20:47:42 +0300

    Megabytes converter added.

--- src/com/mek/MegaBytesConverter.java
+++ src/com/mek/MegaBytesConverter.java
@@ -2,8 +2,8 @@ package com.mek;
 
 public class MegaBytesConverter {
 
-    public static void printMegaBytesAndKiloBytes(int kiloBytes){
-        System.out.println((kiloBytes<0) ? "Invalid Value" : kiloBytes + " KB = "+kiloBytes/1024+" MB and "+kiloBytes%1024+" KB");
+    public static void printMegaBytesAndKiloBytes(int kiloBytes) {
+        System.out.println((kiloBytes < 0) ? "Invalid Value" : kiloBytes + " KB = " + kiloBytes / 1024 + " MB and " + kiloBytes % 1024 + " KB");
     }
 
 
