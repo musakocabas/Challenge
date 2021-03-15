2de66092307286a3bc2490b7f2f0e32bc7f33bda
Musa Kocabas <mek26mek@gmail.com>
2021-03-15 16:41:28 +0300

    EqualSumChecker added.

--- src/com/mek/BarkingDog.java
+++ src/com/mek/BarkingDog.java
@@ -2,6 +2,6 @@ package com.mek;
 
 public class BarkingDog {
     public static boolean shouldWakeUp(boolean isBarking,int hourOfDay){
-        return (isBarking && ((hourOfDay>=0 && hourOfDay<8) || (hourOfDay>22 && hourOfDay<=23))) ? true : false;
+        return isBarking && ((hourOfDay>=0 && hourOfDay<8) || hourOfDay == 23);
     }
 }
